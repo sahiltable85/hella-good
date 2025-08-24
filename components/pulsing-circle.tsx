625ca5bc7b69a40474de0ed04a5941590d938f68
sahiltable85 <sahil@table-85.com>
2025-08-24 23:50:04 +0400

    Update pulsing-circle.tsx

--- components/pulsing-circle.tsx
+++ components/pulsing-circle.tsx
@@ -48,7 +48,7 @@ export default function PulsingCircle() {
           </defs>
           <text className="text-sm fill-white/80 instrument">
             <textPath href="#circle" startOffset="0%">
-              home of the Smashburger • home of the Smashburger • home of the Smashburger • home of the Smashburger •
+              home of the Smashburger • home of the Smashburger •
             </textPath>
           </text>
         </motion.svg>
