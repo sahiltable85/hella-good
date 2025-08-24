fd4efbc1b320dc5a233923a58035567523d99013
sahiltable85 <sahil@table-85.com>
2025-08-24 23:00:52 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,6 @@
-"use client";
-
-import Header from "@/components/header";
-import HeroContent from "@/components/hero-content";
-import VideoBackground from "@/components/video-background";
-import PulsingCircle from "@/components/pulsing-circle";
-import BackgroundVideo from "./components/BackgroundVideo";
+// app/page.tsx
+import HomeClient from "./home-client";
 
 export default function Page() {
-  return (
-    <main>
-      <BackgroundVideo src="/video/hero.mp4" poster="/video/hero.jpg" />
-      <VideoBackground>
-        <Header />
-        <HeroContent />
-        <PulsingCircle />
-      </VideoBackground>
-    </main>
-  );
+  return <HomeClient />;
 }
